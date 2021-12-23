36172f1a6bddb56257b84317d575a14cbdd52d51
jgranick <jmgranick@gmail.com>
2021-12-23 16:53:39 -0500

    Remove scrap alternate attempt file

--- 2021/Day 10/10.1.py
+++ 2021/Day 10/10.1.py
@@ -2,10 +2,10 @@
 ####PART 1##########################################
 ###############------Import data----------------####
 from collections import defaultdict
-with open("test_input.txt", "r", encoding="utf-8") as f:
+with open("input.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
 
-print(lines)
+#print(lines)
 delimiters = [("(", ")"), ("[", "]"), ("{", "}"), ("<", ">") ]
 opening = []
 closing = []
@@ -15,49 +15,41 @@ for delim in delimiters:
     opening.append(open_delim)
     closing.append(close_delim)
     expected_delim[open_delim] = close_delim
-
 scores = {")" : 3, "]" : 57, "}" : 1197, ">" : 25137, "not_corrupt" : 0}
-#print(opening)
-#print(closing)
 
-def corrupt(chunk, opener = '', close = '',opened = [], closed = [], predict_close = [], opened_delimiter = True, open_count = 0, close_count = 0):
+def corrupt(chunk, opened = [], closed = [],  error = False):
     pass
-    if chunk == []:
-        opened_delimiter = False
-        return "not_corrupt" 
-    elif close != '' and  expected_delim[opener] != close:
-        if open_count == close_count:
-            opened_delimiter = False
-        return [opened, closed, predict_close, opened_delimiter, open_count, close_count]
+    if not chunk or error == True:
+        closing_delimiter = closed.pop(0)
+        return [opened, closed,  error, closing_delimiter]
     else:
         if chunk[0] in opening:
-            open_count += 1
             opener = chunk[0]
-            opened.append(opener)
-            predict_close.insert(0, expected_delim[opener])
-
+            opened.insert(0, opener)
         if chunk[0] in closing:
-            close_count += 1
             close = chunk[0]
+            opener = opened.pop(0)
+            if close != expected_delim[opener]:
+                error = True
             closed.insert(0, close)
-        return corrupt(chunk[1:], opener, close, opened, closed, predict_close, opened_delimiter)
-
+        return corrupt(chunk[1:], opened, closed, error)
 
 score = 0
 for chunk in lines:
-    opened , closed, predicted, opened_delimiter, opened, closed = corrupt(chunk)
-    print("Chunk: ", chunk)
-    print ("Open?: ", opened_delimiter)
+    opened , closed,  error, closing_delim = corrupt(chunk)
+    print("Chunk: ", chunk.strip())
     print("Opened: ", opened)
     print("Closed: ", closed)
-    print("Predicted: ", predicted)
-    print("Open: ", opened)
-    print("Closed: ", closed)
+    print("Delimiter wrong: ", error)
+    print("Delimeter used : ", closing_delim)
+    if error == True:
+        score += scores[closing_delim]
 
-    ##score += scores[corrupt(chunk)]
-    #print(score)
+    print("Score: ", score, "\n")
 
-#print(score)
+    
+
+print(score)
 
 
 
