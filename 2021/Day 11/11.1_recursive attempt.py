8b3226f8672ac187df9c8f5e1ad2d4dcd8eb1384
jgranick <jmgranick@gmail.com>
2021-12-27 17:41:30 -0500

    Day 11 working input part 1

--- 2021/Day 11/11.1_recursive attempt.py
+++ 2021/Day 11/11.1_recursive attempt.py
@@ -3,7 +3,7 @@
 #########------Import data----------------####
 from collections import defaultdict
 from io import IncrementalNewlineDecoder
-with open("test_input.txt", "r", encoding="utf-8") as f:
+with open("input.txt", "r", encoding="utf-8") as f:
     lines = f.readlines()
 octopi = []
 for line in lines:
@@ -14,63 +14,63 @@ for line in lines:
     octopi.append(row)
 print(octopi)
 
-def incr_flash_adjacent(coord, energy):
+def incr_flash_adjacent(coord, energy, flashed = []):
     new_flashes = []
     x = coord[0]
     y = coord[1] 
     energy[coord] = 0
-    if (x+1, y) in energy.keys() and energy[(x+1, y)] <= 9:
+    if (x+1, y) in energy.keys() and energy[(x+1, y)] <= 9 and (x+1, y) not in flashed:
         energy[(x+1, y)] +=1
-        if energy[(x+1, y)] == 9: new_flashes.append((x+1, y))
+        if energy[(x+1, y)] >= 9: new_flashes.append((x+1, y))
     
-    if (x-1, y) in energy.keys() and energy[(x-1, y)] <= 9:
-        energy[(x - 1, y)] += 1
-        if energy[(x-1, y)] == 9: new_flashes.append((x-1, y))
+    if (x-1, y) in energy.keys() and energy[(x-1, y)] <= 9 and (x-1, y) not in flashed:
+        energy[(x-1, y)] += 1
+        if energy[(x-1, y)] >= 9: new_flashes.append((x-1, y))
     
-    if (x, y+1) in energy.keys() and energy[(x, y+1)] <= 9:
-        energy[(x, y + 1)] +=1
-        if energy[(x, y+1)] == 9: new_flashes.append((x, y+1))
+    if (x, y+1) in energy.keys() and energy[(x, y+1)] <= 9 and (x, y+1) not in flashed:
+        energy[(x, y+1)] +=1
+        if energy[(x, y+1)] >= 9: new_flashes.append((x, y+1))
     
-    if (x, y-1) in energy.keys() and energy[(x, y-1)] <= 9: 
+    if (x, y-1) in energy.keys() and energy[(x, y-1)] <= 9 and (x, y-1) not in flashed: 
         energy[(x, y-1)] +=1
-        if energy[(x, y-1)] == 9: new_flashes.append((x, y-1))
+        if energy[(x, y-1)] >= 9: new_flashes.append((x, y-1))
     
-    if (x+1, y-1) in energy.keys() and energy[(x+1, y-1)] <= 9: 
+    if (x+1, y-1) in energy.keys() and energy[(x+1, y-1)] <= 9 and (x+1, y-1) not in flashed: 
         energy[(x+1, y-1)] += 1
-        if energy[(x+1, y-1)] == 9: new_flashes.append((x+1, y-1))
+        if energy[(x+1, y-1)] >= 9: new_flashes.append((x+1, y-1))
     
-    if (x+1, y+1) in energy.keys() and energy[(x+1, y+1)] <= 9:
+    if (x+1, y+1) in energy.keys() and energy[(x+1, y+1)] <= 9 and (x+1, y+1) not in flashed:
         energy[(x+1, y+1)]+=1
-        if energy[(x+1, y+1)] == 9: new_flashes.append((x+1, y+1))
+        if energy[(x+1, y+1)] >= 9: new_flashes.append((x+1, y+1))
     
-    if (x-1, y-1) in energy.keys() and energy[(x-1, y-1)] <= 9:
+    if (x-1, y-1) in energy.keys() and energy[(x-1, y-1)] <= 9 and (x-1, y-1) not in flashed:
         energy[(x -1, y-1)]+=1
-        if energy[(x-1, y-1)] == 9: new_flashes.append((x-1, y-1))
+        if energy[(x-1, y-1)] >= 9: new_flashes.append((x-1, y-1))
     
-    if (x - 1, y + 1) in energy.keys() and energy[(x - 1, y + 1)] <= 9:
+    if (x - 1, y + 1) in energy.keys() and energy[(x - 1, y + 1)] <= 9 and (x - 1, y + 1) not in flashed:
         energy[(x - 1, y + 1)] +=1
-        if energy[(x - 1, y + 1)] == 9: new_flashes.append((x - 1, y + 1))
+        if energy[(x - 1, y + 1)] >= 9: new_flashes.append((x - 1, y + 1))
     return (energy, new_flashes)
 
 def flash_adj(coords, energy, flashed = []):
     if coords == []:
-        return energy
+        return [energy, flashed]
     else:
         coord = coords.pop(0)
         if coord not in flashed:
             flashed.append(coord)
-            energy, new_coords = incr_flash_adjacent(coord, energy)
+            energy, new_coords = incr_flash_adjacent(coord, energy, flashed)
+            energy[coord] = 0
             coords = coords + new_coords
+            print("flashed: ", flashed)
+            print_grid(energy)
         return flash_adj(coords, energy, flashed)
 
 
-def incr_energy(energy, flashed):
-    for coord in energy:
-        if coord not in flashed:
+def incr_energy(energy, initial_flash):
+    for coord in energy.keys():
+        if coord not in initial_flash:
             energy[coord] += 1
-        else:
-            coord = 0
-
     return energy
 
 def scan_energy(octopi):
@@ -80,7 +80,6 @@ def scan_energy(octopi):
             energy_levels[(x, y)] = octopi[y][x]
     return energy_levels
 
-#needs a rewrite
 def new_flashes(energy, flashed = []):
     flashing_octopi = []
     for coord in energy.keys() :
@@ -91,78 +90,58 @@ def new_flashes(energy, flashed = []):
 def identity(x):
     return x
 
-def flash_reaction(energy, flashed = []):
-    flashing_energy = new_flashes(energy)
-    flash_adjacent = flash_adj(flashing_energy, energy, flashed)
-    flashed = flashing_energy
+def flash_reaction(energy, flashed=[]):
+    flash_adjacent, flashed = flash_adj(new_flashes(energy), energy, flashed)
     print("Flash adjacent: ", flash_adjacent)
     return [flash_adjacent, flashed]
 
-def flash_cycles(cycles = 0, energy = {}, n = 0):
-    if n == cycles:
+
+#Need to set up initialincrement 
+def flash_cycles(cycles = 0, energy = {}, n = 0, flash_count = 0):
+    if n ==cycles: 
+        print("Cycle: ", n)
+        print("Flashes: ", flash_count)
+        print_grid(energy)
         return energy
     else:
-        energy, flashed = flash_reaction(energy)
-        print("Postreactionns: ", energy)
-        incremented_energy = incr_energy(energy, flashed)
+        print("First set: ", n)
+        #print_grid(energy)
+        energy, flashed = flash_reaction(energy, flashed = [])
+        energy = incr_energy(energy, flashed)
+        print("Flashed: ", flashed)
+        flash_count += len(flashed)
         n+=1
-    return flash_cycles(cycles, incremented_energy, n)
+    return flash_cycles(cycles, energy, n, flash_count)
+
+def print_grid(octo_energy):
+    max_y = 0
+    max_x = 0
+    for key in octo_energy.keys():
+        x = key[0]
+        y = key[1]
+        if x > max_x: max_x = x
+        if y > max_y: max_y = y
+    grid = []
+    blanks = []
+    for y in range(0, max_y+1):
+        blanks.append(0)
+    for x in range(0, max_x+1):
+        grid.append(blanks.copy())
+    for y in range(0, max_y+1):
+        for x in range(0, max_x+1):
+            grid[y][x] = octo_energy[(x,y)]
+    for row in grid:
+        print(row)
 
 
 #create initials scan of energy levels
 octo_energy = scan_energy(octopi)
-print("Octopi starting conditions:") 
-for octopus, energ in octo_energy.items():
-    print("Octopus: ",octopus, "Energy:  ", energ)
+flash_cycles(cycles = 100, energy = octo_energy)
 
-#Scan for flashes
-
-flashing_energy = new_flashes(octo_energy)
-print("Octopi that will flash:") 
-for octopus in flashing_energy:
-    print("Octopus: ",octopus, "Energy:  ", octo_energy[octopus])
-
-octo_energy = scan_energy(octopi)
-
-print("Octopi conditions after flash:")
-print(flash_reaction(octo_energy))
-#print(flashing_energy)
-#coord = flashing_energy.pop(0)
-#print(coord)
-#print(incr_flash_adjacent(coord, energy))
-post_flash = flash_adj(flashing_energy, octo_energy)
-for octopus, energ in post_flash.items():
-    print("Octopus: ",octopus, "Energy:  ", energ)
+#print(new)
 
 
 
-octo_energy = flash_cycles(cycles = 2, energy = octo_energy)
-print(octo_energy)
-
-max_y = 0
-max_x = 0
-for key in octo_energy.keys():
-    x = key[0]
-    y = key[1]
-    if x > max_x: max_x = x
-    if y > max_y: max_y = y
-print(max_x, max_y)
-
-grid = []
-blanks = []
-for y in range(0, max_y+1):
-    blanks.append(0)
-print(blanks)
-
-for x in range(0, max_x+1):
-    grid.append(blanks.copy())
-
-for y in range(0, max_y+1):
-    for x in range(0, max_x+1):
-        print(octo_energy[(x,y)])
-
-for row in grid:
-    print(row)
 
 
 
